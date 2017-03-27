298853d2d7809378d5d796568c2671b15061f4cd
Olaf Lüke <olaf@tinkerforge.com>
2017-03-27 16:04:27 +0200

    Add stub software for XMC1400 breakout

---
+++ software/src/communication.h
@@ -0,0 +1,54 @@
+/* xmc1400-breakout-bricklet
+ * Copyright (C) 2017 Olaf Lüke <olaf@tinkerforge.com>
+ *
+ * communication.h: TFP protocol message handling
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef COMMUNICATION_H
+#define COMMUNICATION_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "bricklib2/bootloader/bootloader.h"
+
+// Default functions
+BootloaderHandleMessageResponse handle_message(const void *data, void *response);
+void communication_tick(void);
+void communication_init(void);
+
+// Constants
+
+
+// Function and callback IDs and structs
+
+
+
+
+// Function prototypes
+
+
+// Callbacks
+
+
+#define COMMUNICATION_CALLBACK_TICK_WAIT_MS 1
+#define COMMUNICATION_CALLBACK_HANDLER_NUM 0
+#define COMMUNICATION_CALLBACK_LIST_INIT \
+
+
+#endif
