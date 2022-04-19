719993177aa1416e1b36f600fefa70db2d400997
Erik Fleckstein <erik@tinkerforge.com>
2022-04-19 16:04:42 +0200

    examples: uc: Rename TF_HalContext to TF_HAL.

--- software/examples/uc/example_gpio.c
+++ software/examples/uc/example_gpio.c
@@ -9,19 +9,19 @@
 
 void check(int rc, const char* msg);
 
-void example_setup(TF_HalContext *hal);
-void example_loop(TF_HalContext *hal);
+void example_setup(TF_HAL *hal);
+void example_loop(TF_HAL *hal);
 
 
 static TF_XMC1400Breakout xb;
 
-void example_setup(TF_HalContext *hal) {
+void example_setup(TF_HAL *hal) {
 	// Create device object
 	check(tf_xmc1400_breakout_create(&xb, UID, hal), "create device object");
 
 	// Set Port 1, Pin 0 alternating high/low for 5 times with 1s delay
 	int i;
-	for(i = 0; i < 5; ++i) {
+	for (i = 0; i < 5; ++i) {
 		tf_hal_sleep_us(hal, 1000 * 1000);
 		check(tf_xmc1400_breakout_set_gpio_config(&xb, 1, 0,
 		                                          TF_XMC1400_BREAKOUT_GPIO_MODE_OUTPUT_PUSH_PULL,
@@ -33,7 +33,7 @@ void example_setup(TF_HalContext *hal) {
 	}
 }
 
-void example_loop(TF_HalContext *hal) {
+void example_loop(TF_HAL *hal) {
 	// Poll for callbacks
 	tf_hal_callback_tick(hal, 0);
 }
